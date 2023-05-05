9f682dd6fd004d884db5493e8707327aa57b0ebe
KingofTheCourt <joerex.thambaiah@gmail.com>
2023-05-05 12:19:59 -0400

    changed function

--- backend/main.py
+++ backend/main.py
@@ -6,19 +6,13 @@ from findRestobyGroceries import findRestobyGroceries
 
 app = Flask(__name__, template_folder="../frontend/build", static_folder="../frontend/build/static")
 
-@app.route('/')
+@app.route('/') // creating website
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 
 @app.route('/api/FindResto')
-def video_feed():
+def findResto():
     return findRestobyGroceries("Test")
 
 if __name__ == '__main__':
