b89b8dcea156e54fb17418c5ce0a65d878bc8013
KingofTheCourt <joerex.thambaiah@gmail.com>
2023-05-07 19:12:43 -0400

    testChange

--- backend/findRestobyGroceries.py
+++ backend/findRestobyGroceries.py
@@ -6,10 +6,10 @@ def findRestobyGroceries(grocery):
     for resto in dictionaryStore:
         if grocery in dictionaryStore[resto]:
             list.append(resto)
-
+    #test
     return list 
     
-
+#[{name:restoName,latitude:number,longitude:numberTwo},{name:restoName,latitude:number,longitude:numberTwo}]
 
 if __name__ == "__main__":
     print(findRestobyGroceries("pineapple"))
